e4a8926cb42687821addffbaba3c624bdb0ad358
paolabrito <paolakta@hotmail.com>
2019-10-19 10:19:32 -0300

    adicionando vizualização do produto

---
+++ vizprod.php
@@ -0,0 +1,31 @@
+<?php include_once("header.php"); ?>
+
+    <main>
+      <section class='vizprod'>
+        <div class='vizprod_container' >
+          <div class='virprod_card'>
+            
+            <img src="img/livro_placeholder.png" alt="imagem do livro">
+          
+            <ul>
+              <li class='book'>LIVRO 1</li>
+              <li class='author'>Sobrenome, Nome</li>
+              <li class='price'> R$ XXXX,XX</li>
+              <button class='add_carrinho'>Adicionar ao carrinho</button> 
+
+            </ul>
+          </div>
+        </div>
+
+        <div class='sinopse'>
+          <h1>SINOPSE</h1>
+          <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ea provident minus dicta consectetur porro dolorum at, maiores, architecto distinctio commodi voluptates nihil necessitatibus quod inventore reprehenderit accusantium, magnam debitis!</div>
+        </div>
+        <div class='detail'>
+            <h1>DETALHES DO PRODUTO</h1>
+          <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, repellendus ad nam quo id aliquid harum iure doloribus provident, adipisci corrupti magni molestiae. Veritatis atque dolorum maiores, aliquid asperiores voluptas?</div>
+        </div>
+        <?php include_once("footer.php"); ?>
+      </section>
+    
+    </main>
