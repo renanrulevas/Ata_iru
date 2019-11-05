697f577477557f096c57a23d44be81f346feb163
Regiane Machado <regi.sabbath@gmail.com>
2019-11-04 22:48:41 -0300

    Atualização da tela de login - Responsivo com bootstrap

--- conta.php
+++ conta.php
@@ -1,30 +1,30 @@
 <?php include_once("header.php"); ?>
-    <main>
-      <section class="box">
-      <div class="main">
+    <main class="main-account">
+      <section class="box-account">
+      <div class="div-account">
         <h1 class="title-account">Sua Conta</h1>
-          <div class="application">
-            <div class="main-icon">
-                <span><i class="fas fa-box-open main-icons"></i></span>
+          <div class="account">
+            <div class="account-icon">
+                <span><i class="fas fa-box-open"></i></span>
             </div>
-            <div class="main-txt">
+            <div class="account-txt">
               <h2 class="title-account-2">Seus Pedidos</h2>
               <p>Ver pedidos, rastrear ou cancelar compras</p>
             </div>
           </div>
           <div class="account">
-            <div class="main-icon">
-              <span><i class="fas fa-users-cog main-icons"></i></span>
+            <div class="account-icon">
+              <span><i class="fas fa-users-cog"></i></span>
             </div>
-            <div class="main-txt">
+            <div class="account-txt">
               <h2 class="title-account-2">Alterar Conta</h2>
               <p>Modificar senha e Informações pessoais</p>
             </div>
           </div>
       </div>
     </section>
-    <div class="main-icon-prev">
-      <div class="main-prev">
+    <div class="account-icon-prev">
+      <div class="account-prev">
         <span><i class="fas fa-angle-double-left icon-prev"></i></span>
       </div>
     </div>
--- login.php
+++ login.php
@@ -1,29 +1,31 @@
 <?php include_once("header.php"); ?>
-
-    <main>
-      <section>
-        <div class="form-conta">
-                <form action="" method="POST" class="">
-                    <h1 class="form-color" id="form-color-title">Login</h1>
-                    <div class="">
-                        <div class="form-icon">
-                            <span class="form-color" id=""><i class="fas fa-user"></i></span>
-                        </div>
-                        <input class="form-input" type="text" name="username" placeholder="Digite seu e-mail ou nome de usuário" required autofocus>*
-                    </div>
-                    <div class="">
-                        <div class="form-icon">
-                            <span class="form-color" id=""><i class="fas fa-lock"></i></span>
-                        </div>
-                        <input class="form-input" type="password" name="password" placeholder="Digite sua senha"required>*
-                    </div>
-                    <div class="form-link">
-                      <a href="" class="">Esqueceu a Senha?</a>
-                      <a href="" class="">Ainda não sou cadatrado</a>
-                    </div>
-                    <button class="btn" type="submit">Enviar <i class="fas fa-arrow-alt-circle-right"></i></button>
-                </form>
-        </div>
-      </section>
-    </main>
+<main class="container-fluid" id="main-login">
+  <section class="row justify-content-center">
+    <section class="col-12 col-sm-6 col-md-5">
+      <form id="form-container-login">
+        <div class="form-group">
+          <div class="login-div-icones  text-left">
+            <label for="login-email">
+                <span><i class="fas fa-user"></i></span> E-mail ou usuário *
+            </label>
+         </div>
+         <input type="email" class="form-control" id="login-email" aria-describedby="emailHelp" placeholder="Digite seu usuário ou e-mail" name="login-email">
+       </div>
+       <div class="form-group">
+        <div class="login-div-icones text-left">
+          <label for="login-senha">
+              <span><i class="fas fa-lock"></i></span> Senha *
+         </label>
+       </div>
+       <input type="password" class="form-control" id="login-senha" placeholder="Digite sua senha" name="login-senha">
+     </div>
+     <div class="form-group" id="login-links">
+       <a href="" class="">Esqueceu a Senha?</a>
+       <a href="" class="">Ainda não sou cadatrado</a>
+     </div>
+     <button type="submit" class="btn btn-primary btn-block" id="botao-login">Enviar</button>
+   </form>
+ </section>
+</section>
+</main>
 <?php include_once("footer.php"); ?>
