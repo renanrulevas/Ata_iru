95af0676dd8dd96b78ecfa551cb2e2d1de873eab
paolabrito <paolakta@hotmail.com>
2019-11-05 23:01:53 -0300

    alterando a vizprod

--- vizprod.php
+++ vizprod.php
@@ -1,31 +1,45 @@
 <?php include_once("header.php"); ?>
+  <section>
+    <div class="container-fluid">
+      <div class="row">
 
-    <main>
-      <section class='vizprod'>
-        <div class='vizprod_container' >
-          <div class='virprod_card'>
-            
-            <img src="img/livro_placeholder.png" alt="imagem do livro">
-          
-            <ul>
-              <li class='book'>LIVRO 1</li>
-              <li class='author'>Sobrenome, Nome</li>
-              <li class='price'> R$ XXXX,XX</li>
-              <button class='add_carrinho'>Adicionar ao carrinho</button> 
+        <div class="col col-7 mt-5 mr-md-3">
+        
+          <img src="img/livro_placeholder.png" class= 'img-fluid rounded mx-auto d-block' alt="imagem do livro">
+        </div>
 
-            </ul>
+        <div class="col col-4 mt-5" text-align="start">
+          <div class="row">
+            <h1>Livro 1</h1>
           </div>
-        </div>
-
-        <div class='sinopse'>
-          <h1>SINOPSE</h1>
-          <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error ea provident minus dicta consectetur porro dolorum at, maiores, architecto distinctio commodi voluptates nihil necessitatibus quod inventore reprehenderit accusantium, magnam debitis!</div>
-        </div>
-        <div class='detail'>
-            <h1>DETALHES DO PRODUTO</h1>
-          <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, repellendus ad nam quo id aliquid harum iure doloribus provident, adipisci corrupti magni molestiae. Veritatis atque dolorum maiores, aliquid asperiores voluptas?</div>
-        </div>
-        <?php include_once("footer.php"); ?>
-      </section>
     
-    </main>
+          <div class="row">
+            <h3>Nome, Sobrenome</h3>
+          </div>
+          
+          <div class="row">
+          R$XXXX,XX
+          </div>
+          
+          <div class="row mt-1">
+            <button type="button" class="btn btn-success">Adicionar ao carrinho</button>
+          </div>
+          
+        </div>
+      </div>
+      <div class="row m-5">
+      <h3>SINOPSE</h3>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae ullam et aspernatur, iure, corrupti assumenda quas vel minima laborum iste accusantium magnam voluptas mollitia non temporibus sit qui magni doloribus.
+      </div>
+      <div class="row m-5">
+      <h3>DETALHES DO PRODUTO</h3>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, beatae pariatur obcaecati placeat dolor ullam recusandae commodi, explicabo veritatis minima modi. Consequatur ipsum totam, autem illum corporis incidunt in neque.
+      </div>
+
+
+    </div>
+  </section>
+  
+
+
+<?php include_once("footer.php"); ?>
