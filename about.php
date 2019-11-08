5073697efc4fa0f807aaec928a7abb9818a6641b
valt3r <valter.rodriguez@outlook.com.br>
2019-11-07 21:06:02 -0300

    reponsividade na pagina

--- about.php
+++ about.php
@@ -1,26 +1,46 @@
 <?php include_once("header.php"); ?>
-        
+
+<div class="container">
     <main>
+        <div class="row">
+            <div class="col">
+                <h1>Quem somos</h1>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <section>
+                        <img src="img/about.jpeg" class="img-fluid" alt="">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt architecto autem ea minus
+                            quaerat
+                            nostrum nisi doloremque non doloribus sequi iste illum, similique ipsa alias ipsum rem
+                            perferendis
+                            atque amet.
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, repellendus
+                            necessitatibus!
+                            Blanditiis maxime tempore sunt natus et incidunt quae, quas aperiam ab facilis. Excepturi
+                            recusandae, in dolor voluptatibus quis consequuntur.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt architecto autem ea minus
+                            quaerat
+                            nostrum nisi doloremque non doloribus sequi iste illum, similique ipsa alias ipsum rem
+                            perferendis
+                            atque amet.
+                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae neque odio maxime
+                            voluptas
+                            ullam ipsam dolores quis cumque voluptatum, quas alias perspiciatis commodi aut omnis autem,
+                            molestias aliquam veniam earum.
+                        </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt architecto autem ea minus
+                            quaerat
+                            nostrum nisi doloremque non doloribus sequi iste illum, similique ipsa alias ipsum rem
+                            perferendis
+                            atque amet.</p>
+                    </section>
+                </div>
+            </div>
+        </div>
 
-    <h1>Quem somos</h1>
-    <section>
-    <img src="img/about.jpeg" alt="">
-    <br>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt architecto autem ea minus quaerat nostrum nisi doloremque non doloribus sequi iste illum, similique ipsa alias ipsum rem perferendis atque amet.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt architecto autem ea minus quaerat nostrum nisi doloremque non doloribus sequi iste illum, similique ipsa alias ipsum rem perferendis atque amet.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt architecto autem ea minus quaerat nostrum nisi doloremque non doloribus sequi iste illum, similique ipsa alias ipsum rem perferendis atque amet.</p>
-    </section>
-        <!-- >> CONTEÚDO AQUI <<
+    </main>
+</div>
 
-        <ul style="list-style: none; color: red; font-weight: bold; text-align: left; padding-top: 20px; width: 60%; margin: 0 auto;">
-            <li>1) Crie uma cópia dessa página</li>
-            <li>2) Renomeie de acordo com a página que vc for fazer (Exemplo: de index.php para pagina.php)</li>
-            <li>3) Agora é só colocar todo o conteúdo dentro da tag < main ></li>
-            <li>4) Se quiser criar um arquivo css só para sua página, crie o arquivo na pasta css e não se esqueça de adicionar o arquivo no header.php</li>
-            <li>5) Coloquei um form de exemplo no arquivo form_exemplo.php, mas ele usa bootstrap. O link do bootstrap nesse exemplo tá dentro do main pq é só pra teste mesmo (por isso ele tá ferrando o footer e o menu dropdown). Para facilitar a criação do form poderiamos pensar em algum framework tipo bootstrap/materialize/semantic ui</li>
-        </ul> -->
-        
-
-    </main>        
-        
 <?php include_once("footer.php"); ?>
