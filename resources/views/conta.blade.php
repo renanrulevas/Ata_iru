b94208a9278d6f9868e05f352044dcd16681118b
Regiane Machado <regi.sabbath@gmail.com>
2020-04-07 20:39:25 -0300

    Retirado pedidos no conta cliente

--- resources/views/conta.blade.php
+++ resources/views/conta.blade.php
@@ -5,19 +5,6 @@
 <main id="conta-principal">
   <section class="container">
     <h1>Conta</h1>
-    <div class="container conta-div-principal mb-2">
-      <div class="row justify-content-center">
-        <div class="col-8 col-sm-6 col-lg-6">
-          <div class="form-group">
-            <img src="../img/cart.svg" alt="logotipo usuário">
-          </div>
-        </div>
-        <div class="col-8 col-sm-6 col-lg-6">
-          <h2 class="title-conta-2 titulo-conta-2">Seus Pedidos</h2>
-          <p class="paraf-conta"><a href="/carrinho">Ver pedidos, rastrear ou cancelar compras</a></p>
-        </div>
-      </div>
-    </div>
     <div class="container conta-div-principal">
       <div class="row justify-content-center">
         <div class="col-12 col-sm-6 col-lg-6">
